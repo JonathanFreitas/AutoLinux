8c608d90c31b52d261faaf007ae112be3153587d
JonathanFreitas <34845116+JonathanFreitas@users.noreply.github.com>
2023-08-11 11:49:01 -0300

    Create mount_ram_memory.sh

---
+++ mount_ram_memory.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo -n "Quantos GB de RAM: "
+read valor
+mkdir /var/swap
+fallocate -l $valorG /var/swap/swapfile
+chmod 600 /var/swap/swapfile
+mkswap /var/swap/swapfile
+swapon  /var/swap/swapfile
+cp /etc/fstab /etc/fstab.bak
+echo '/var/swap/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
