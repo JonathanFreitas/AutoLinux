6b09746b6118369e4800a95f1e5e1d8159e45c4f
JonathanFreitas <34845116+JonathanFreitas@users.noreply.github.com>
2023-09-27 17:09:36 -0300

    Create remove_images_docker.sh

---
+++ remove_images_docker.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+mkdir /scripts
+cat <<-EOF > /scripts/remove_images_docker.sh
+#!/bin/bash
+###########################################
+#
+# clean/remove all container/images
+#
+# The script will
+#  - remove images
+#
+
+
+# remove all images
+echo '####################################################'
+echo 'Removing images ...'
+echo '####################################################'
+docker rmi $(docker images -q)
+EOF
+chmod 777 /scripts/remove_images_docker.sh
+
+(crontab -l; echo "0 22 * * * /scripts/remove_images_docker.sh >/dev/null 2>&1") | sort -u | crontab -
