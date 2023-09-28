930f9645b565095533fec0e043cadc6158a8a49a
JonathanFreitas <34845116+JonathanFreitas@users.noreply.github.com>
2023-09-28 16:44:26 -0300

    Update backup_dir_to_s3.sh

--- backup_dir_to_s3.sh
+++ backup_dir_to_s3.sh
@@ -10,27 +10,27 @@ S3_BUCKET="bkp-mercadolegal-lightsail"
 S3_PREFIX="$NAME_SERVER"
 
 # Nome do arquivo ZIP a ser criado
-ZIP_FILENAME="$NAME_SERVER.zip"
+ZIP_FILENAME="$NAME_SERVER"
 
 # Compacta os arquivos em um arquivo ZIP
-zip -r "$WORK_DIR/$ZIP_FILENAME" "$SOURCE_DIR"
+ls $SOURCE_DIR >/tmp/ARQ.txt
+while read I;do
+  tar -zvcf "$WORK_DIR/$ZIP_FILENAME.tar.gz" "$SOURCE_DIR/$I"
+  TAMANHO=`du -ch "$WORK_DIR/$ZIP_FILENAME.tar.gz" | grep total | awk -F" " '{print $1}'`
+  echo -e "$I-$DATA.tar.gz ... $TAMANHO\n" >> /tmp/log.txt
+done </tmp/ARQ.txt
+rm -Rf /tmp/ARQ.txt
 
 # Verifique se a compactação foi bem-sucedida
-if [ $? -eq 0 ]; then
-  echo "Arquivos foram compactados com sucesso em $ZIP_FILENAME."
+
+echo "Arquivos foram compactados com sucesso em $ZIP_FILENAME."
   
-  # Envia o arquivo ZIP para o S3
-  aws s3 cp "$WORK_DIR/$ZIP_FILENAME" "s3://$S3_BUCKET/$S3_PREFIX/$ZIP_FILENAME"
+# Envia o arquivo ZIP para o S3
+aws s3 cp "$WORK_DIR/$ZIP_FILENAME.tar.gz" "s3://$S3_BUCKET/$S3_PREFIX/$ZIP_FILENAME"
   
-  # Verifique se o envio para o S3 foi bem-sucedido
-  if [ $? -eq 0 ]; then
-    echo "Arquivo $ZIP_FILENAME foi enviado com sucesso para o S3."
+# Verifique se o envio para o S3 foi bem-sucedido
+
+echo "Arquivo $ZIP_FILENAME foi enviado com sucesso para o S3."
     
-    # Remova o arquivo ZIP local após o envio
-    rm "$WORK_DIR/$ZIP_FILENAME"
-  else
-    echo "Erro ao enviar o arquivo $ZIP_FILENAME para o S3."
-  fi
-else
-  echo "Erro ao compactar os arquivos em $ZIP_FILENAME."
-fi
+# Remova o arquivo ZIP local após o envio
+#rm "$WORK_DIR/$ZIP_FILENAME.tar.gz"
