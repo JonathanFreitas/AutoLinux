525837077b025d85a8493782faceadb0c87e5603
JonathanFreitas <34845116+JonathanFreitas@users.noreply.github.com>
2023-12-04 13:46:41 -0300

    Update remove_images_docker.sh

--- remove_images_docker.sh
+++ remove_images_docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mkdir /scripts
+mkdir -p /scripts
 cat <<'EOF' > /scripts/remove_images_docker.sh
 #!/bin/bash
 ###########################################
@@ -19,4 +19,5 @@ docker rmi $(docker images -q)
 EOF
 chmod 777 /scripts/remove_images_docker.sh
 
-{ crontab -l; echo "0 23 * * * /scripts/remove_images_docker.sh >/dev/null 2>&1"; } | crontab -
+{ crontab -l; echo "10 8-18 * * * /scripts/remove_images_docker.sh >/dev/null 2>&1"; } | crontab -
+{ crontab -l; echo "10 2 * * * /scripts/remove_images_docker.sh >/dev/null 2>&1"; } | crontab -
