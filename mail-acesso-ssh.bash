084ee25664041f16b70b200f65a4c5e9db158ddd
JonathanFreitas <34845116+JonathanFreitas@users.noreply.github.com>
2023-12-11 16:05:33 -0300

    Update mail-acesso-ssh.bash

--- mail-acesso-ssh.bash
+++ mail-acesso-ssh.bash
@@ -15,24 +15,24 @@ sudo cat <<'EOF' > /etc/profile.adm
 # EMAIL DE AVISO DE ACESSO
 #
 rootalert() {
-echo 'ALERTA - Acesso SHELL'
-echo
-echo 'Servidor: 'hostname
-echo 'IP do servidor: ' wget http://ipecho.net/plain -O - -q
-echo 'Data: 'date
-echo 'Usuario: 'last -1 | grep logged | awk '{ print $1 }'
-echo 'TTY: 'last -1 | grep logged | awk '{ print $2 }'
-echo 'Origem: 'last -1 | grep logged | awk '{ print $3 }'
-echo
-echo 'Estes usuários estão ativos neste instante:'
-echo who | awk '{print $1}'
-echo
-echo 'Últimos 10 acessos efetuados:'
-echo last -n 10
-echo
-echo 'Informações: Horário deste acesso, Uptime e Load Averange atual'
-echo uptime
-echo
+  echo 'ALERTA - Acesso SHELL'
+  echo
+  echo 'Servidor: '$(hostname)
+  echo 'IP do servidor: '$(wget -qO - http://ipecho.net/plain)
+  echo 'Data: '$(date)
+  echo 'Usuario: '$(last -1 | grep logged | awk '{ print $1 }')
+  echo 'TTY: '$(last -1 | grep logged | awk '{ print $2 }')
+  echo 'Origem: '$(last -1 | grep logged | awk '{ print $3 }')
+  echo
+  echo 'Estes usuários estão ativos neste instante:'
+  who | awk '{print $1}'
+  echo
+  echo 'Últimos 10 acessos efetuados:'
+  last -n 10
+  echo
+  echo 'Informações: Horário deste acesso, Uptime e Load Averange atual'
+  uptime
+  echo
 }
 rootalert | mail -s "Alerta: Acesso via SSH [hostname]" monitoramento.aws@emepar.com.br jonathan@grupo2ag.com.br thiago@delend.finance
 EOF
